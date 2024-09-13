48870aca15b289fa3e3e59d399378eea13226326
pablorenan-dev <pablonichterwistz@outlook.com>
2024-09-13 19:53:04 -0300

    adicionado função de mostrar itens do cadapio

--- cardapio/cardapioScript.js
+++ cardapio/cardapioScript.js
@@ -1,3 +1,28 @@
-function mostrarItemDoCardapio() {
-  console.log("nada ainda");
+const header = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+async function GETItensCardapio() {
+  let response = await fetch("http://localhost:5164/api/CardapioItems", {
+    method: "GET",
+    headers: header,
+  });
+  let result = await response.json();
+  console.log(result);
+  montarItensCardapio(result);
 }
+
+function montarItensCardapio(cardapioItens) {
+  let ulCardapioItens = document.querySelector("ul");
+  cardapioItens.forEach((item) => {
+    ulCardapioItens.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li>${item.titulo}</li>
+      `
+    );
+  });
+}
+
+GETItensCardapio();
