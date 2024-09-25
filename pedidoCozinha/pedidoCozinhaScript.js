79dcb7afb2bfedb1845e0ab2c99514f5c752407e
GansoGamer01 <guilhermeougui01@gmail.com>
2024-09-25 20:26:59 -0300

    padronizado o código e tela com o resto do projeto

--- pedidoCozinha/pedidoCozinhaScript.js
+++ pedidoCozinha/pedidoCozinhaScript.js
@@ -3,7 +3,7 @@ const header = {
     "Content-Type": "application/json",
   };
 
-  async function GETItensCardapio() {
+  async function GETPedidoCozinha() {
     let response = await fetch("https://localhost:7129/api/CardapioItems", {
       method: "GET",
       headers: header,
@@ -11,28 +11,30 @@ const header = {
     let result = await response.json();
     console.log(result);
     montarPedidoCozinha(result);
-    adicionarEventoCliqueMoverItemAndamento();
+    // adicionarEventoCliqueMoverItemAndamento();
   }
   
   function montarPedidoCozinha(cardapioItens) {
-    let ulPedidoCozinhaItensAFazer = document.querySelector("#ulPendente");
+    let ulPedidoCozinhaItensAFazer = document.querySelector("#ul-Pendente");
     cardapioItens.forEach((item) => {
         ulPedidoCozinhaItensAFazer.insertAdjacentHTML(
         "beforeend",
         `
-        <li>${item.titulo}</li>
+        <li>
+        <p>${item.titulo}</p>
+        <button>→</button>
+        </li>
         `
       );
     });
   }
-  GETItensCardapio();
+  GETPedidoCozinha();
 
-function adicionarEventoCliqueMoverItemAndamento(cardapioItens)
-{
-  const btnMoverItemAndamento = document.querySelector("#mover-andamento");
-  btnMoverItemAndamento.addEventListener("click", () => {
-    cardapioItens.splice;
-    // mandar pra api a atuliazação do status do item q tu clicou
-    // monta a lista novamente
-  });
-}
+// function adicionarEventoCliqueMoverItemAndamento(cardapioItens)
+// {
+//   const btnMoverItemAndamento = document.querySelector("#mover-andamento");
+//   btnMoverItemAndamento.addEventListener("click", () => {
+//     // mandar pra api a atuliazação do status do item q tu clicou \\
+//     // monta a lista novamente \\
+//   });
+// }
