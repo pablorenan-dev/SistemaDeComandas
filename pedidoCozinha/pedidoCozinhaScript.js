42609920565b760118f0518b83e520b50c1eb4c5
GansoGamer01 <guilhermeougui01@gmail.com>
2024-10-02 22:02:06 -0300

    adicionado a função de PUT + integração do PUT aos buttons da tela de PedidoCozinha

--- pedidoCozinha/pedidoCozinhaScript.js
+++ pedidoCozinha/pedidoCozinhaScript.js
@@ -3,39 +3,74 @@ const header = {
     "Content-Type": "application/json",
   };
 
-  async function GETPedidoCozinha() {
-    let response = await fetch("https://localhost:7129/api/PedidoCozinhas", {
-      method: "GET",
-      headers: header,
-    });
-    let result = await response.json();
-    console.log(result);
-    montarPedidoCozinhaPendentes(result);
-    // adicionarEventoCliqueMoverItemAndamento();
-  };
+async function GETPedidoCozinha(situacaoId,element) {
+  //ENDPOINT == ?situacaoId
+  // situacaoId = 1 == pendente; situacaoId = 2 == andamento; situacaoId = 3 == finalizado;
+  let response = await fetch(`https://localhost:7129/api/PedidoCozinhas?situacaoId=${situacaoId}`, {
+    method: "GET",
+    headers: header,
+  });
+  let result = await response.json();
+  console.log(result);
+  montarPedidoCozinha(result,element,situacaoId);
+};
   
-  function montarPedidoCozinhaPendentes(pedidosPendentes) {
-    let ulPedidoCozinhaItensAFazer = document.querySelector("#ul-Pendente");
-    pedidosPendentes.forEach((pedido) => {
-        ulPedidoCozinhaItensAFazer.insertAdjacentHTML(
-        "beforeend",
-        `
-        <li>
-        <p>${pedido.item}</p>
-        <button>→</button>
-        </li>
-        `
-      );
-    });
-  };
+function montarPedidoCozinha(pedidos,element,finish) {
+  console.log(finish,"finish")
+  let ulPedidoCozinhaItens = document.querySelector(element);
+  ulPedidoCozinhaItens.innerHTML = ""
+  pedidos.forEach((pedido) => {
+    ulPedidoCozinhaItens.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li>
+      <p>${pedido.item}</p>
+      
+      ${ finish !=3 ? `<button id=${`btn-mover${pedido.id}`}>→</button>` : ''}
+      
+      </li>
+      `
+    );
+    const btnMover = document.getElementById(`btn-mover${pedido.id}`)
+   
+    if(btnMover){
+      btnMover.addEventListener("click",()=>PUTPedidoCozinha(pedido.id,finish))
+    }
+  });
+};
   
-  GETPedidoCozinha();
+GETPedidoCozinha(1,"#ul-Pendente");
+GETPedidoCozinha(2,"#ul-Andamento");
+GETPedidoCozinha(3,"#ul-Finalizado");
 
-// function adicionarEventoCliqueMoverItemAndamento(cardapioItens)
-// {
-//   const btnMoverItemAndamento = document.querySelector("#mover-andamento");
-//   btnMoverItemAndamento.addEventListener("click", () => {
-//     // mandar pra api a atuliazação do status do item q tu clicou \\
-//     // monta a lista novamente \\
-//   });
-// }
+async function PUTPedidoCozinha(id,situacaoId) {
+  
+  const body = {
+    novoStatusId:situacaoId+1
+  }
+  let response = await fetch(`https://localhost:7129/api/PedidoCozinhas/${id}`, {
+    method: "PUT",
+    headers: header,
+    body:JSON.stringify(body)
+  });
+
+  // let result = await response.json();
+  console.log(response);
+  if(response.ok){
+    GETPedidoCozinha(1,"#ul-Pendente");
+    GETPedidoCozinha(2,"#ul-Andamento");
+    GETPedidoCozinha(3,"#ul-Finalizado");
+  }
+  };
+
+
+function adicionarEventoCliqueMoverItemAndamento(id)
+{
+  const btnMoverItemAndamento = document.getElementById("#mover-andamento");
+  btnMoverItemAndamento.addEventListener("click", () => {
+
+  });
+}
+
+PUTPedidoCozinha(2);
+PUTPedidoCozinha(3);
