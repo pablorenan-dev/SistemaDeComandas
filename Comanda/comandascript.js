c5c89e4d134b8d948ba10f84bfe8e8d150cd5586
pablorenan-dev <pablonichterwistz@outlook.com>
2024-10-31 21:06:04 -0300

    ajeitado o css das telas: mesa, cardapio, usuario e comanda

--- Comanda/comandascript.js
+++ Comanda/comandascript.js
@@ -290,3 +290,13 @@ const icons = {
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
   </svg>`,
 };
+
+// Adiciona um evento de clique no h1 para retornar para o menu
+function adicionarEventoCliqueH1Chiquinho() {
+  const h1Chiquinho = document.querySelector(".h1-chiquinho");
+  h1Chiquinho.addEventListener("click", () => {
+    document.location.href = "http://127.0.0.1:5500/index.html";
+  });
+}
+
+adicionarEventoCliqueH1Chiquinho();
